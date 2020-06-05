f5750a13edf96af4958872e0e0698ba9513aeb2c
Vito Kortbeek <vito.kortbeek@gmail.com>
2020-06-05 13:38:16 +0200

    Update mock function example

--- src/plugins/mock_function_plugin/MockFunction.cpp
+++ src/plugins/mock_function_plugin/MockFunction.cpp
@@ -12,7 +12,7 @@
 #include <atomic>
 
 #include "icemu/emu/Emulator.h"
-#include "icemu/hooks/HookCode.h"
+#include "icemu/hooks/HookFunction.h"
 #include "icemu/hooks/HookManager.h"
 #include "icemu/hooks/RegisterHook.h"
 #include "icemu/emu/Function.h"
@@ -22,57 +22,48 @@ using namespace std;
 using namespace icemu;
 
 
-class MockFunction : public HookCode {
- private:
-  string printLeader() {
-    return "(icemu) ";
-  }
-
-  string func_name;
+class MockFunction : public HookFunction {
 
  public:
   // Always execute
   MockFunction(Emulator &emu, string fname)
-      : HookCode(emu, "mock_function_" + fname,
-                 emu.getMemory().getSymbols().get(fname)->getFuncAddr()),
-        func_name(fname) {}
+      : HookFunction(emu, fname) {}
 
   ~MockFunction() {
   }
 
   // Hook run
   void run(hook_arg_t *arg) {
-    cout << "Func: " << func_name << " at: " << arg->address << endl;
+    cout << "Func: " << function_name << " at: " << arg->address << endl;
 
     //cout << "Registers:" << endl;
     //getEmulator().getRegisters().dump(cout);
     Function::Argument<uint32_t> farg1;
+    // Example changing the parse lambda function:
     //farg1.parse = [&farg1](const char *virtregbuff) {
     //  std::cout << "Copy" << std::endl;
     //  memcpy((void *)&farg1.arg, virtregbuff, farg1.size);
     //};
     Function::Argument<uint16_t> farg2;
-    Function::Arguments::parse(getEmulator().getRegisters(), farg1, farg2);
+    Function::Arguments::parse(getRegisters(), farg1, farg2);
 
     cout << "  Argument: " << farg1.arg << ", " << farg2.arg << endl;
 
-    Function::setReturn(getEmulator().getRegisters(), 42);
-
-    //Function::skip();
-    Registers &reg = getEmulator().getRegisters();
-    reg.set(Registers::PC, reg.get(Registers::LR));
+    Function::setReturn(getRegisters(), 42);
+    Function::skip(getRegisters());
+    // Same as:
+    // Function::skip(getRegisters(), 42);
   }
 };
 
 // Function that registers the hook
 static void registerMyCodeHook(Emulator &emu, HookManager &HM) {
-  std::string fname = "call_simple";
-  try {
-    auto mf = new MockFunction(emu, fname);
-    HM.add(mf);
-  } catch (...) {
-    cerr << "Failed to register mock hook for:" << fname << endl;
+  auto mf = new MockFunction(emu, "call_simple");
+  if (mf->getStatus() == Hook::STATUS_ERROR) {
+    delete mf;
+    return;
   }
+  HM.add(mf);
 }
 
 // Class that is used by ICEmu to finf the register function
