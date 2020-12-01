121b9c12c61838ffe0211165f9ae55333fa47e9b
Vito Kortbeek <vito.kortbeek@gmail.com>
2020-12-01 12:50:51 +0100

    [Registers] Add APSR to the printed list (as R16)
    Note, this is not xPSR, unicorn does not seem to have a public API
    to get xPSR.
    
    TODO: either add to unicorn, or combine the PSR registers myself
    TODO2: Add names to the registers (as PSR is not "R16")

--- include/icemu/emu/Registers.h
+++ include/icemu/emu/Registers.h
@@ -32,14 +32,15 @@ class Registers {
     SP = UC_ARM_REG_SP,
     PC = UC_ARM_REG_PC,
     LR = UC_ARM_REG_LR,
+    APSR = UC_ARM_REG_APSR,
     RETURN = R0,
   };
 
  private:
   uc_engine *uc_ = NULL;
 
-  const std::array<enum reg, 16> regmap = {
-      R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15};
+  const std::array<enum reg, 17> regmap = {
+      R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, APSR};
 
  public:
   inline void init(uc_engine *uc) { uc_ = uc; }
