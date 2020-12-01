dd33fe72ffc2cd1054f04425a3fc5597fbec5483
Vito Kortbeek <vito.kortbeek@gmail.com>
2020-12-01 12:50:02 +0100

    [HookControlRegisters] Add verbose flag
    Now always false

--- include/icemu/hooks/builtin/HookControlRegisters.h
+++ include/icemu/hooks/builtin/HookControlRegisters.h
@@ -13,13 +13,15 @@ namespace icemu {
 class HookControlRegisters : public HookMemory {
  private:
   const std::string memory_segment_name = "SYSTEM_CONTROL_REGISTERS";
+  bool verbose = false;
 
 
  public:
   HookControlRegisters(Emulator &emu) : HookMemory(emu, "ctrl-reg") {
     type = TYPE_RANGE;
     // Need to modify some options
-    std::cout << "Get the memory segment" << std::endl;
+    if (verbose)
+      std::cout << "Get the memory segment" << std::endl;
     memseg_t *memseg = emu.getMemory().find(memory_segment_name);
     if (memseg == NULL) {
       std::cerr << "Did not find the memory segment: " << memory_segment_name << std::endl;
@@ -41,19 +43,23 @@ class HookControlRegisters : public HookMemory {
     armaddr_t addr = arg->address;
 
     if (arg->mem_type == HookMemory::MEM_READ) {
-      std::cout << "[CTRL-REGISTER] read" << std::endl;
+      if (verbose) std::cout << "[CTRL-REGISTER] read" << std::endl;
       if (addr == 0xE000ED08) {
-        std::cout << "VTOR: " << arg->value << std::endl;
+        if (verbose) std::cout << "VTOR: " << arg->value << std::endl;
       }
     } else if (arg->mem_type == HookMemory::MEM_WRITE) {
-      std::cout << "[CTRL-REGISTER] write" << std::endl;
+      if (verbose) std::cout << "[CTRL-REGISTER] write" << std::endl;
       if (addr == 0xE000ED08) {
-        std::cout << "VTOR: " << arg->value << std::endl;
+        if (verbose) std::cout << "VTOR: " << arg->value << std::endl;
       }
     } else {
-      std::cout << "[CTRL-REGISTER] UNKNOWN TYPE" << std::endl;
+      if (verbose) std::cout << "[CTRL-REGISTER] UNKNOWN TYPE" << std::endl;
       //getEmulator().stop();
     }
+  }
+
+  void setVerbose(bool v) {
+    verbose = v;
   }
 };
 }
