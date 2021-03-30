1bb1297b3dd911995b3dc4ac97a03a68667dbab5
Vito Kortbeek <vito.kortbeek@gmail.com>
2021-03-30 15:45:27 +0200

    [Plugin][CycleCount] Add a (guestimated) cycle count
    Assumes that the prediction cost follows this persons estimate:
    https://stackoverflow.com/a/28774579
    
    It does sound reasonable.
    
    TODOS:
    * Divisions are not correct (do we care? They are probably rare enough).
    * Combining LOAD and STORE addresses into less cycles if they follow
      each other. This might have a bigger influence, but impossible to tell
      how ARM actually did it.

--- src/plugins/cycle_count_plugin/CycleCount.cpp
+++ src/plugins/cycle_count_plugin/CycleCount.cpp
@@ -7,6 +7,7 @@
  * Should be compiled as a shared library, i.e. using `-shared -fPIC`
  */
 #include <iostream>
+#include <vector>
 
 #include "capstone/capstone.h"
 
@@ -22,45 +23,17 @@ using namespace icemu;
 class Pipeline {
  private:
 
-  bool pipeline_cost = false;
-  armaddr_t addr_prev_instruction = 0;
+  uint64_t expected_next_instruction_addr = 0;
 
- public:
-  uint64_t cycle_count = 0;
+  // Store the IMM operands of the last instruction
+  vector<armaddr_t> last_instr_immediates;
 
-  // Add a capstone instruction to the pipeline
-  void add(cs_insn *insn) {
-
-    // Indexes into the arm_insn table in capstone
-    arm_insn instruction = (arm_insn)insn->id;
-
-    // Get the operands (if it touches the PC we might need to refil the
-    // pipeline)
-    auto *cs_arm = &insn->detail->arm;
-    int operand_count = cs_arm->op_count;
-    auto *operands = cs_arm->operands;
-
-    int P = 3; // pipeline refill cost
+  int cost(arm_insn instruction, int operand_count) {
     int instruction_cost = 1;
 
     // Approximate
     // Info from: https://developer.arm.com/documentation/ddi0439/b/Programmers-Model/Instruction-set-summary/Cortex-M4-instructions
     switch (instruction) {
-      case ARM_INS_MOV:
-      case ARM_INS_MOVT:
-      case ARM_INS_MOVW:
-      case ARM_INS_ADD:
-      case ARM_INS_ADC:
-      case ARM_INS_ADR:
-      case ARM_INS_SUB:
-      case ARM_INS_SBC:
-      case ARM_INS_RSB:
-      case ARM_INS_SUBW:
-        if ((operand_count > 0) && (operands[0].reg == ARM_REG_PC)) {
-          instruction_cost = 1 + P;
-        }
-        break;
-
       case ARM_INS_MLA:
       case ARM_INS_MLS:
         instruction_cost = 2;
@@ -88,35 +61,158 @@ class Pipeline {
       case ARM_INS_LDRBT:
       case ARM_INS_LDRSHT:
       case ARM_INS_LDRSBT:
-        if ((operand_count > 0) && (operands[0].reg == ARM_REG_PC)) {
-          instruction_cost = 2 + P;
-        }
+        instruction_cost = 2;
         break;
 
       // Double word loads
+      case ARM_INS_LDRD:
+        instruction_cost = 1+2; // 1+N N=2
+        break;
+
       case ARM_INS_LDM:
-        instruction_cost = 1 + operand_count;
-        {
-          for (int i=0; i<operand_count; i++) {
-            if (operands[i].reg == ARM_REG_PC) {
-              instruction_cost += P;
-              break;
-            }
-          }
-        }
-        cout << "Using LDM instruction with " << operand_count << " operands" << endl;
+        instruction_cost = 1 + (operand_count-1); // cost is 1+N where N is the register list (number of op -1)
         break;
 
       // Stores
-      case ARM_INS_STORE
+      case ARM_INS_STR:
+      case ARM_INS_STRBT:
+      case ARM_INS_STRB:
+      case ARM_INS_STRH:
+      case ARM_INS_STRHT:
+      case ARM_INS_STRT:
+        instruction_cost = 2; // just as load, it can be compacted to 1 TODO
+        break;
 
+      case ARM_INS_STRD:
+        instruction_cost = 1+2; // 1+N N=2
+        break;
+
+      case ARM_INS_STM:
+        instruction_cost = 1 + (operand_count-1); // cost is 1+N where N is the register list (number of op -1)
+        break;
+
+      case ARM_INS_PUSH:
+      case ARM_INS_POP:
+        instruction_cost = 1 + operand_count;
+        break;
+
+      case ARM_INS_LDREX:
+      case ARM_INS_LDREXH:
+      case ARM_INS_LDREXB:
+      case ARM_INS_STREX:
+      case ARM_INS_STREXH:
+      case ARM_INS_STREXB:
+        instruction_cost = 2;
+        break;
+
+      case ARM_INS_TBB:
+      case ARM_INS_TBH:
+        instruction_cost = 2;
+        break;
+
+      case ARM_INS_ISB:
+        instruction_cost = 1 + 3;
+        break;
+
+      case ARM_INS_DMB:
+      case ARM_INS_DSB:
+        instruction_cost = 1; // this is implementation specific
+        break;
 
       default:
         // already set the default
         break;
     }
+    return instruction_cost;
   }
 
+  // Returns the pipeline cost of goind from the previous instruction to this
+  // one. So we estimate it after the fact.
+  int pipleline_cost(cs_insn *insn) {
+    // If the previous address is futher away than one instruction we jumped.
+    // SO that means the pipeline was reset (non-taken branch is predicted and
+    // does not requre a refill of the pipeline).
+    // Branch prediction is magic, but we going to assume this is correct enough:
+    // https://stackoverflow.com/questions/28760617/pipeline-refill-cycles-for-instructions-in-arm
+    //
+    // So if the target (this_address, because we are doing this after the fact)
+    // * is 32-bit halfword-aligned it's 3 cycles
+    // * is 32-bit and word-alighned it's 2 cycles
+    // * is 16-bit it's 2 cycles
+    // * is predicted it's 1 cycle: the branch causing instruction was
+    //    unconditional AND had the immediate value of the current address.
+    int P = 0;
+
+    uint64_t this_address = insn->address;
+    // Check if we jumped
+    if (this_address != expected_next_instruction_addr) {
+      // if thhis address
+      bool static_jump = false;
+      for (const auto imm : last_instr_immediates) {
+        if (this_address == imm) {
+          // We jumped to a constant immediate, so we assume that ARM can
+          // predict the jump. i.e., P=1
+          P = 1;
+          static_jump = true;
+          //cout << "Predicted the jump!" << endl;
+          break;
+        }
+      }
+
+      if (!static_jump) {
+        if (insn->size == 4) {
+          if (!(this_address & 0b11)) {
+            P = 2;  // word aligned
+          } else {
+            P = 3;  // not word aligned
+          }
+        } else {
+          // 16-bit instruction
+          P = 2;
+        }
+      }
+    }
+    return P;
+  }
+
+ public:
+  uint64_t cycle_count = 0;
+
+  // Add a capstone instruction to the pipeline
+  int add(cs_insn *insn) {
+
+    // Indexes into the arm_insn table in capstone
+    arm_insn instruction = (arm_insn)insn->id;
+
+    // Get the operands (if it touches the PC we might need to refil the
+    // pipeline)
+    auto *cs_arm = &insn->detail->arm;
+    int operand_count = cs_arm->op_count;
+    uint64_t this_address = insn->address;
+
+    int cycles = cost(instruction, operand_count);
+    int P = pipleline_cost(insn);
+
+    cycle_count += cycles + P;
+
+    // Prepare for the next instruction
+    expected_next_instruction_addr = this_address + insn->size;
+
+    last_instr_immediates.clear();
+    // The jump can be predicted IF the jump is unconditional
+    // AND if the destination is an immediate.
+    // Note: this is a guess, ARM never revealed how it does branch prediction
+    if (cs_arm->cc == ARM_CC_AL) {
+      auto *operands = cs_arm->operands;
+      for (int i=0; i<operand_count; i++) {
+        auto *op = &operands[i];
+        if (op->type == ARM_OP_IMM) {
+          last_instr_immediates.push_back((armaddr_t)op->imm);
+        }
+      }
+    }
+    return cycles + P;
+  }
 };
 
 class CycleCount : public HookCode {
@@ -129,13 +225,14 @@ class CycleCount : public HookCode {
 
  public:
   // Always execute
-  CycleCount(Emulator &emu) : HookCode(emu, "display_instructions") {
+  CycleCount(Emulator &emu) : HookCode(emu, "cycle_count") {
   }
 
   ~CycleCount() {
+    cout << "The program ran for: " << pipeline.cycle_count << " clock cycles (estimate)" << endl;
   }
 
-  void displayInstruction(armaddr_t address, armaddr_t size) {
+  void countCycles(armaddr_t address, armaddr_t size) {
     bool ok;
     uint8_t instruction[size];
 
@@ -151,20 +248,23 @@ class CycleCount : public HookCode {
       return;
     }
 
-    // Display the actual instruction
+    pipeline.add(insn);
+
+    // Display the instruction
+#if 0
     for (size_t i=0; i<cnt; i++) {
       cout << printLeader() << " ";
       printf("0x%08x: %s  %s", (armaddr_t)insn[i].address, insn[i].mnemonic, insn[i].op_str);
       cout << endl;
     }
+#endif
 
-    pipeline.add(insn);
     cs_free(insn, cnt);
   }
 
   // Hook run
   void run(hook_arg_t *arg) {
-    displayInstruction(arg->address, arg->size);
+    countCycles(arg->address, arg->size);
   }
 };
 
