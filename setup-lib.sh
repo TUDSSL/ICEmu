2248806302f9f3e0e95b0a3a14831c28a295669c
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-07-11 15:34:56 +0200

    Update setup lib script

--- setup-lib.sh
+++ setup-lib.sh
@@ -9,22 +9,16 @@ pushd lib
 
 pushd unicorn
 echo "Build unicorn"
-#UNICORN_ARCHS="arm" UNICORN_QEMU_FLAGS="--python=/usr/bin/python2" ./make.sh
-./make.sh
-popd
-
-pushd capstone
-echo "Build capstone"
-CAPSTONE_ARCHS="arm" ./make.sh
-popd
-
-pushd jsoncpp
-echo "Build jsoncpp"
 mkdir -p build
 pushd build
 cmake ../
 make -j4
 popd
+popd
+
+pushd capstone
+echo "Build capstone"
+./make.sh
 popd
 
 popd
