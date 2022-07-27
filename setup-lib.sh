5c8838dc24847d861c4948b828362b9578598f2a
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-07-27 11:35:37 +0200

    Update library setup

--- setup-lib.sh
+++ setup-lib.sh
@@ -3,19 +3,19 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $BASE_DIR
 
-git submodule update --init --recursive
-
 pushd lib
 
+git submodule update --init --recursive unicorn
 pushd unicorn
 echo "Build unicorn"
 mkdir -p build
 pushd build
 cmake ../
-make -j4
+make -j$(nproc)
 popd
 popd
 
+git submodule update --init --recursive capstone
 pushd capstone
 echo "Build capstone"
 ./make.sh
